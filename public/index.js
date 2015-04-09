844b8e646308966b84cbc9adc16aade6a11a1b59
Cfo613 <cforan13@gmail.com>
2015-04-08 22:41:20 -0400

    updated styling

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function() {
 
   // get image element
-  var elem = document.getElementById("background");
+  var elem = document.getElementById("indexBody");
 
   // get rectangle coordinates
   var rect = elem.getBoundingClientRect();
@@ -12,13 +12,33 @@ window.addEventListener('load', function() {
   var top = rect.top;
   var width = rect.width;
 
-  // get village1 element and set coordinates
-  var village1 = document.getElementById("village1");
-  village1.setAttribute("coords", "0," + 0.6*height.toString() + "," + 0.3*width.toString() + "," + 0.9*height.toString());
+  // // get village1 element and set coordinates
+  // var village1 = document.getElementById("village1");
+  // village1.setAttribute("coords", "0," + 0.6*height.toString() + "," + 0.3*width.toString() + "," + 0.9*height.toString());
+  //
+  // // get city1 element and set coordinates
+  // var city1 = document.getElementById("city1");
+  // // city1.setAttribute("coords", "100,0,400,300");
+  // city1.setAttribute("coords", 0.3*width.toString() + ",0," + 0.7*width.toString() + "," + 0.5*height.toString());
 
-  // get city1 element and set coordinates
-  var city1 = document.getElementById("city1");
-  // city1.setAttribute("coords", "100,0,400,300");
-  city1.setAttribute("coords", 0.3*width.toString() + ",0," + 0.7*width.toString() + "," + 0.5*height.toString());
+  // set padding for city button (will respond to size of div and therefore size of screen)
+  var cityTopPx = 0.20*height.toString() + "px";
+  var cityLeftPx = 0.72*width.toString() + "px";
+  document.getElementById("cityDiv").style.paddingTop = cityTopPx;
+  document.getElementById("cityDiv").style.paddingLeft = cityLeftPx;
+
+  // set padding for settlement button (will respond to size of div and therefore size of screen)
+  var settlementTopPx = 0.25*height.toString() + "px";
+  var settlementLeftPx = 0.55*width.toString() + "px";
+  document.getElementById("settlementDiv").style.paddingTop = settlementTopPx;
+  document.getElementById("settlementDiv").style.paddingLeft = settlementLeftPx;
+
+  // set padding for wilds button (will respond to size of div and therefore size of screen)
+  var wildsTopPx = 0.15*height.toString() + "px";
+  var wildsLeftPx = 0.10*width.toString() + "px";
+  document.getElementById("wildsDiv").style.paddingTop = wildsTopPx;
+  document.getElementById("wildsDiv").style.paddingLeft = wildsLeftPx;
+
+
 
 });
--- public/table.js
+++ public/table.js
@@ -14,25 +14,25 @@ window.addEventListener('load', function() {
 
   // set padding for back button (will respond to size of div and therefore size of screen)
   var backTopPx = 0.02*height.toString() + "px";
-  var backLeftPx = 0.15*width.toString() + "px";
+  var backLeftPx = 0.05*width.toString() + "px";
   document.getElementById("backDiv").style.paddingTop = backTopPx;
   document.getElementById("backDiv").style.paddingLeft = backLeftPx;
 
   // set padding for mission button (will respond to size of div and therefore size of screen)
-  var missionTopPx = 0.10*height.toString() + "px";
-  var missionLeftPx = 0.23*width.toString() + "px";
+  var missionTopPx = 0.08*height.toString() + "px";
+  var missionLeftPx = 0.27*width.toString() + "px";
   document.getElementById("missionDiv").style.paddingTop = missionTopPx;
   document.getElementById("missionDiv").style.paddingLeft = missionLeftPx;
 
   // set padding for letter button (will respond to size of div and therefore size of screen)
-  var letterTopPx = 0.40*height.toString() + "px";
+  var letterTopPx = 0.15*height.toString() + "px";
   var letterLeftPx = 0.75*width.toString() + "px";
   document.getElementById("letterDiv").style.paddingTop = letterTopPx;
   document.getElementById("letterDiv").style.paddingLeft = letterLeftPx;
 
   // set padding for rumor button (will respond to size of div and therefore size of screen)
-  var rumorTopPx = 0.05*height.toString() + "px";
-  var rumorLeftPx = 0.20*width.toString() + "px";
+  var rumorTopPx = 0.08*height.toString() + "px";
+  var rumorLeftPx = 0.37*width.toString() + "px";
   document.getElementById("rumorDiv").style.paddingTop = rumorTopPx;
   document.getElementById("rumorDiv").style.paddingLeft = rumorLeftPx;
 
